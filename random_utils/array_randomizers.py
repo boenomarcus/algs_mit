2a773992b0bb0a43ac8d283bedffef4be6f37fcc
Marcus M. Boeno <42239994+boenomarcus@users.noreply.github.com>
2020-10-28 16:36:29 -0300

    Randomize in Place Approach to Randomize Arrays

--- random_utils/array_randomizers.py
+++ random_utils/array_randomizers.py
@@ -33,21 +33,53 @@ def _permute_by_sorting(A:list) -> list:
     return [x for _,x in sorted(zip(P, A))]
 
 
-def array_randomize(A:list, method="permute_sort") -> list:
-    """Rearrange Array Elements
+def _randomize_inPlace(A:list) -> list:
+    """Permute Array Elements without Random Priorities
 
     Big-O Notation:
-        - "permute_sort" yields "n*lg(n)" time complexity.
-        
+        - Randomize in Place yields "n" (linear) time complexity.
+    
     > Arguments:
         - A (list): Array to be randomly rearranged.
     
     > Output:
         - Rearranged array.
     """
-    # Recursive Algorithm
+    # Create a deep copy of A
+    X = A[:]
+
+    # Iterating over array elements
+    for i in range(len(X)):
+        j = random.randint(0, len(X)-1)
+        X[i], X[j] = X[j], X[i]
+    
+    # Return rearranged array
+    return X
+
+
+def array_randomize(A:list, method="in_place") -> list:
+    """Rearrange Array Elements
+
+    Big-O Notation:
+        - "permute_sort" yields "n*lg(n)" time complexity;
+        - "in_place" yields "n" (linear) time complexity
+        
+    > Arguments:
+        - A (list): Array to be randomly rearranged.
+        - method (str): Method to get randomized array.
+            ---> Options: "permute_sort", "in_place".
+            ---> Defaults to "in_place".
+
+    > Output:
+        - Rearranged array.
+    """
+    # Permute Array Elements by Sorting Random Priorities
     if method == "permute_sort":
         return _permute_by_sorting(A)
+    
+    # Permute Array Elements without Random Priorities
+    elif method == "in_place":
+        return _randomize_inPlace(A)
         
     # Method not implemented
     else:
@@ -56,11 +88,14 @@ def array_randomize(A:list, method="permute_sort") -> list:
 
 if __name__ == "__main__":
 
-    # Declare arrays and get randomized rearrangements
+    # Declare array and get randomized rearrangements
     A = list(range(11))
     
     print("\n>> Array Randomizer Examples:")
     print(f"\nOriginal Array: {A}")
     
     # Permute Array Elements by Sorting Random Priorities
-    print(f"\n   > Permute by Sorting: {array_randomize(A, 'permute_sort')}\n")
+    print(f"\n   > Permute by Sorting: {array_randomize(A, 'permute_sort')}")
+    
+    # Permute Array Elements without Random Priorities
+    print(f"   > Randomize in Place: {array_randomize(A, 'in_place')}\n")
